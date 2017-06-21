ae3507bbd9671ad5e9dcf27a95d8b55892a6d71d
Sergio Mrtin <sergio.ms.91@gmail.com>
2017-06-21 11:16:57 +0100

    1er Commit Agregada Clase TSP, y modificado el Main para realizar pruebas de su funcionamiento

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Solutions.TSP;
 import graph.Graph;
 
 import java.io.IOException;
@@ -12,8 +13,19 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		if(args.length == 1) {
 			Graph graph = new Graph(args[0]);
-			System.out.println(graph.getName());
+			//System.out.println(graph.getName());
 			//System.out.println(graph.getMatrixItem(1,2));
+
+			TSP solution = new TSP(graph);
+			solution.printData();
+			System.out.println();
+			System.out.println("COST");
+			System.out.println();
+			System.out.println("One Candidate Solution:		"
+					+solution.calculateCostTour(solution.randomSolution()));
+			System.out.println("Other Candidate Solution:	"
+					+solution.calculateCostTour(solution.randomSolution()));
+
 		}
 	}
 }
---
+++ src/Solutions/TSP.java
@@ -0,0 +1,95 @@
+package Solutions;
+
+import java.util.ArrayList;
+import graph.Graph;
+
+public class TSP {
+
+    private Graph mTsp;				// Graph
+
+    /**
+     * Default builder
+     */
+    public TSP(){}
+
+    /**
+     * Builder II
+     * @param tsp
+     */
+    public TSP(Graph tsp){
+        load(tsp);
+    }
+
+    /**
+     * Function to load the file
+     * @param tsp
+     */
+    public void load(Graph tsp){
+        mTsp = tsp;
+    }
+
+    /**
+     * Function to print the graph data.
+     */
+    public void printData(){
+        getTSP().printDataGraph();
+    }
+
+    /**
+     * Function to generate a aleatory solution
+     * @return
+     */
+    public ArrayList<Integer> randomSolution (){
+
+        ArrayList<Integer> nodes = new ArrayList<>();
+        for(int i = 0; i < getTSP().getN(); i++) {
+            nodes.add(i);
+        }
+
+        ArrayList<Integer> randomTour = new ArrayList<> ();
+        do {
+            int indexNode = (int) Math.floor(Math.random() * (getTSP().getN() - randomTour.size()));
+            if(randomTour.size() == 0) {
+                randomTour.add(nodes.get(indexNode));
+            } else {
+                Double cost = getTSP().getMatrixItem(randomTour.get(randomTour.size() - 1), nodes.get(indexNode));
+                if(cost != Double.POSITIVE_INFINITY){
+                    randomTour.add(nodes.get(indexNode));
+                }
+            }
+            nodes.remove(indexNode);
+        } while (randomTour.size() != getTSP().getN());
+        return randomTour;
+    }
+
+    /**
+     * Function to calculate the cost tour
+     * @param tour
+     * @return
+     */
+    public Double calculateCostTour(ArrayList<Integer> tour) {
+        Double cost = 0.0;
+        for(int i = 0; i < tour.size() - 1; i++) {
+            cost += getTSP().getMatrixItem(tour.get(i), tour.get(i + 1));
+        }
+        cost += getTSP().getMatrixItem(tour.get(tour.size() - 1),
+                tour.get(0));
+        return cost;
+    }
+
+    /**
+     * Getter TSP graph
+     * @return
+     */
+    public Graph getTSP() {
+        return mTsp;
+    }
+
+    /**
+     * Setter TSP graph
+     * @param tsp
+     */
+    public void setTSP(Graph tsp) {
+        mTsp = tsp;
+    }
+}
